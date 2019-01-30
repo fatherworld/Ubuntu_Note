4975d7be2c162822930ba29c4264ca84a8491c89
ys <1149144318@qq.com>
2019-01-30 20:29:26 +0800

    经典的机器学习模型

---
+++ 机器学习/第一章线性回归与逻辑回归/理论解释/Logistic Regression_Example/jichu.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import os
+import math
+import random
+if __name__ == '__main__':
+    D3 = dict(name='Bob', age=45)
+    s = set()
+    for g in D3:
+        print("g[0]")
+        print(g[0])
+        print("g[1]")
+        print(g[0])
